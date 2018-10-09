09c806e0403d61de181063f011db493bb3126645
beausimpson <simpson.beau@gmail.com>
2018-10-09 01:41:16 -0500

    modified index.html & style.css, added random score & user score to screen, value added to .crystal-small, on click function displays value in console log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,35 @@
-$("button").on("click", function() {
-    console.log("test")
-})
+
+// global variables for game
+// var computerNumber = 0;
+var playerScore = 0;
+var wins = 0;
+var losses = 0;
+
+
+
+// init function
+function init() {
+    startGame();
+};
+
+// function to (re)start game
+function startGame () {
+    // var radnomNumberGenerator = (Math.floor(Math.random()*(120-19+1)+19));
+    $("#computerNumber").text(randomNumberGenerator(120,19));
+    $("#playerScore").text(playerScore);
+    $(".crystal-small").val(randomNumberGenerator(12,1));
+
+}
+
+$(".crystal-small").click(function() {
+    console.log($(".crystal-small").val())
+});
+
+window.onload = init();
+
+
+
+function randomNumberGenerator(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+};
